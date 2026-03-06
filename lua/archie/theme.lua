f02e200e14916a242571d568fe1f68e43609a8fb
Latif <ibootrc@gmail.com>
2026-03-06 06:55:25 +0100

    fix telescope_color

--- lua/archie/theme.lua
+++ lua/archie/theme.lua
@@ -60,9 +60,9 @@ function M.get(config)
 
     -- TELESCOPE (High Contrast)
     TelescopeNormal = { fg = p.fg, bg = p.none },
-    TelescopeBorder = { fg = p.bg_dark, bg = p.none },
+    TelescopeBorder = { fg = p.fg, bg = p.none },
     TelescopePromptNormal = { fg = p.white, bg = p.none },
-    TelescopePromptBorder = { fg = p.bg_dark, bg = p.none },
+    TelescopePromptBorder = { fg = p.fg, bg = p.none },
     TelescopeSelection = { fg = p.white, bg = p.bg_visual, style = 'bold' },
     TelescopeMatching = { fg = p.cyan, style = 'bold' },
     TelescopePromptPrefix = { fg = p.blue },
