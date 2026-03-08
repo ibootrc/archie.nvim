ee574db4b801d5dcbc6d6c721024f4d432e6dd77
Latif <ibootrc@gmail.com>
2026-03-08 03:54:36 +0100

    variant lazy_text_color

--- lua/archie/theme.lua
+++ lua/archie/theme.lua
@@ -44,7 +44,7 @@ function M.get(config)
     -- CATEGORY 2: DIAGNOSTICS
     DiagnosticError = { fg = p.red },
     DiagnosticWarn = { fg = p.yellow },
-    DiagnosticInfo = { fg = p.fg_dark },
+    DiagnosticInfo = { fg = p.fg_dim, style = 'bold' },
     DiagnosticHint = { fg = p.teal },
 
     -- CATEGORY 1: SYNTAX SPECIFICS
