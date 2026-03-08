b286013d99e615b49cd35d2f4c9aa7bd2dafee2e
Latif <ibootrc@gmail.com>
2026-03-08 04:02:46 +0100

    final v1.0 cyan

--- lua/archie/theme.lua
+++ lua/archie/theme.lua
@@ -44,7 +44,7 @@ function M.get(config)
     -- CATEGORY 2: DIAGNOSTICS
     DiagnosticError = { fg = p.red },
     DiagnosticWarn = { fg = p.yellow },
-    DiagnosticInfo = { fg = p.pink, style = 'bold' },
+    DiagnosticInfo = { fg = p.cyan, style = 'bold' },
     DiagnosticHint = { fg = p.teal },
 
     -- CATEGORY 1: SYNTAX SPECIFICS
